926ea799e07d9cb55ab7fd8297ad7bbf53842c56
geetha <kavita.param999@gmail.com>
2023-05-17 11:16:54 +0530

    springboot application on Kubernetes

---
+++ springboot-demo-kub/src/main/java/com/star/agile/assignment/springbootdemokub/SpringbootDemoKubApplication.java
@@ -0,0 +1,13 @@
+package com.star.agile.assignment.springbootdemokub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDemoKubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDemoKubApplication.class, args);
+	}
+
+}
